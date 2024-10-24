a2c1f933f6684528a1cfb36ccff10235a2c8a63b
d.aikema <djurreaikema@gmail.com>
2024-10-24 14:30:12 +0200

    Scale grid

--- src/app/grid/ui/grid-controls.component.ts
+++ src/app/grid/ui/grid-controls.component.ts
@@ -5,11 +5,38 @@ import {Component, output} from '@angular/core';
   standalone: true,
   imports: [],
   template: `
-    <button (click)="generateMaze.emit()">Generate Maze</button>
-    <button (click)="runAlgorithm.emit()">Run Algorithm</button>
-    <button (click)="resetGrid.emit()">Reset Grid</button>
+    <div class="wrapper">
+      <button (click)="resetGrid.emit()">Reset Grid</button>
+
+      <div class="row">
+        <input type="checkbox"/>
+        <button (click)="generateMaze.emit()">Generate Maze</button>
+      </div>
+
+      <button (click)="runAlgorithm.emit()">Run Algorithm</button>
+
+      <div class="row">
+        <label for="rows">Number of rows: </label><br>
+        <input type="number" id="rows">
+      </div>
+
+      <div class="row">
+        <label for="cols">Number of cols: </label><br>
+        <input type="number" id="cols">
+      </div>
+    </div>
   `,
-  styles: ``
+  styles: [`
+    .wrapper {
+      display: flex;
+      flex-flow: column nowrap;
+    }
+
+    .row {
+      display: flex;
+      flex-flow: row nowrap;
+    }
+  `]
 })
 export class GridControlsComponent {
 
